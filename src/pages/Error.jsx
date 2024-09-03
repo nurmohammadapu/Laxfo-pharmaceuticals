469887aba6a88798f366f91c53c837bd5a03953d
nurmohammadapu <mdapu2003@gmail.com>
2024-09-04 03:24:34 +0600

    Update Error.jsx page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,21 +2,30 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import Navbar from './../components/Navbar';
 import Footer from './../components/Footer';
+import Breadcrumb from './../components/Breadcrumb';
 
 
 
 function Error() {
   return (
 
-    <div className='flex font-Merriweather flex-col gap-3 items-center justify-center  h-screen bg-brown-100'>
+
+    
+
+    <div className='bg-brown-100 font-Merriweather'>
             <Navbar/>
+            <Breadcrumb/>
+            <div className='flex h-screen flex-col gap-5 justify-center items-center text-2xl font-medium'>
             <p className='text-2xl'>
             404 Not Found
             </p>
-                    
             <Link to="/" className='bg-green-300 p-2 rounded-md mx-auto text-white font-medium'>
             Go to Home
             </Link>
+            </div>
+
+                    
+ 
             <Footer/>
         </div>
 
